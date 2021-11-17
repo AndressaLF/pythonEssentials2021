108e848170bcac7f88938b3f7105047ff5cdc774
Andressa Lima Ferreira <andressalimaferreira@gmail.com>
2021-11-17 17:29:10 -0300

    impressão de listas

---
+++ scripts_python/03_3411_impressao_lista.py
@@ -0,0 +1,9 @@
+# Imprimindo um elemento separadamente.
+
+numeros_sorteados = [92, 2, 8, 55, 15, 32, 41, 17, 22, 6, 19, 26, 38, 44, 25]
+print(numeros_sorteados[5])
+
+
+
+# Impressão da lista completa
+print(numeros_sorteados)
