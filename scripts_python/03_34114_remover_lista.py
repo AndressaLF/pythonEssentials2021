9c7f975c86ddef1b17dba41c4ce2a3dc25c52dbe
Andressa Lima Ferreira <andressalimaferreira@gmail.com>
2021-11-17 17:38:06 -0300

    removendo valores da lista

---
+++ scripts_python/03_34114_remover_lista.py
@@ -0,0 +1,7 @@
+numeros_sorteados = [92, 2, 8, 55, 15, 32, 41, 17, 22, 6, 19, 26, 38, 44, 25]
+print(numeros_sorteados)
+print(len(numeros_sorteados))
+
+del numeros_sorteados[0]
+print(len(numeros_sorteados))
+print(numeros_sorteados)
