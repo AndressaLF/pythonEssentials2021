b66d1351d4ec25d4b8e5a8de06e2d07263cc9b28
Andressa Lima Ferreira <andressalimaferreira@gmail.com>
2021-11-17 18:09:22 -0300

    acessando indices negativos

---
+++ scripts_python/03_34115_acessar_lista_indices_negativos.py
@@ -0,0 +1,7 @@
+# Acessando a primeira posição da lista abaixo
+numeros_sorteados = [92, 2, 8, 55, 15] 
+print(numeros_sorteados[-5])  # o valor do elemento da primeira posição é 92
+
+
+# Acessando o elemento da última posição da lista abaixo
+print(numeros_sorteados[-1]) # o valor do elemento da primeira posição é 15
