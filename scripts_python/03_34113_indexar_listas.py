754b81215d937bd2bfa4cb3178ae04b6fe12a2b9
Andressa Lima Ferreira <andressalimaferreira@gmail.com>
2021-11-17 17:37:24 -0300

    atualização

---
+++ scripts_python/03_34113_indexar_listas.py
@@ -0,0 +1,5 @@
+numeros_sorteados = [10, 2, 8, 55, 15, 32, 41, 17, 22, 6, 19, 26, 38, 44, 25]
+print("Lista original: ", numeros_sorteados)
+
+numeros_sorteados[0] = 92
+print("Lista alterada: ", numeros_sorteados)
